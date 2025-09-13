576b64069b02ddcdc988613d394cf13a2ed1e132
DurgaC1 <vijayadurga@1649.com>
2025-09-13 22:24:38 +0530

    Fix JWT

--- server/src/shared/middleware/authMiddleware.ts
+++ server/src/shared/middleware/authMiddleware.ts
@@ -9,7 +9,7 @@ export const protect = async (req: Request, res: Response, next: NextFunction) =
   if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
     token = req.headers.authorization.split(" ")[1];
     try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { id: string };
+      const decoded = jwt.verify(token, process.env.JWT_SECRET as string, {algorithms: ["HS256"],}) as { id: string };
       const user = await User.findById(decoded.id).select("_id name email");
       if (!user) {
         return res.status(401).json({ message: "Not authorized, user not found" });
